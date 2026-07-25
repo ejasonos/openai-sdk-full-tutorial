f23a9b060efa417f9430a5e23fc01e4e95825c45
Favour Ejakpevweoghene Onosemuode <onosejas@gmail.com>
2026-07-25 07:36:12 +0100

    updated file

--- scenario2/fastapi_mcp_calc.py
+++ scenario2/fastapi_mcp_calc.py
@@ -1,5 +1,27 @@
 # HTTP
+'''
+To run the fastapi you only need
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8082)
+
+To convert it to mcp in order to test with npx @modelcontextprotocol/inspector, add the following lines of code to the python file.
+# 2. Converting it to MCP
+mcp = FastApiMCP(app, name='Calculator MCP Converted')
+mcp.mount_http()
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8082)
+
+Then on the mcp ui, set the 
+transport type = Streamable HTTP,
+Url = http://localhost:8082/mcp and 
+click connect
+'''
+
 from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
 
 #1. Let's make a fastapi app
 app = FastAPI(
@@ -37,6 +59,10 @@ def divide(a: float, b: float):
     '''
     result = a/b
     return {'result': result}
+
+# 2. Converting it to MCP
+mcp = FastApiMCP(app, name='Calculator MCP Converted')
+mcp.mount_http()
 
 if __name__ == '__main__':
     import uvicorn
